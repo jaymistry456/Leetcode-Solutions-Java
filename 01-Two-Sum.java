3d70cdc35e0ac59eafee3964c99727cad276e731
jaymistry456 <jay.mistry456@yahoo.com>
2025-12-16 20:13:14 -0400

    day 1 complete

---
+++ 01-Two-Sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // // brute-force
+        // // TC: O(n^2), SC: O(1)
+        // for(int i = 0; i < nums.length; i++) {
+        //     for(int j = i + 1; j < nums.length; j++) {
+        //         if(nums[i] + nums[j] == target) {
+        //             return new int[]{i, j};
+        //         }
+        //     }
+        // }   
+        // return new int[]{-1, -1};
+
+
+        // optimal
+        // // TC: O(n), SC: O(n)
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // num -> index
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
---
+++ 02-Valid-Parentheses.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isValid(String s) {
+        // // brute-force
+        // // TC: O(n^2), SC: O(n)
+        // while(s.contains("()") || s.contains("{}") || s.contains("[]")) {
+        //     if(s.contains("()")) {
+        //         s = s.replace("()", "");
+        //     }
+        //     else if(s.contains("{}")) {
+        //         s = s.replace("{}", "");
+        //     }
+        //     else {
+        //         s = s.replace("[]", "");
+        //     }
+        // }
+        // return s.length() == 0;
+
+
+        // optimal
+        // TC: O(n), SC: O(n)
+        Map<Character, Character> map = new HashMap<>();
+        Stack<Character> stack = new Stack<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        for(Character c: s.toCharArray()) {
+            if(c == ')' || c == '}' || c == ']') {
+                if(stack.isEmpty() || stack.peek() != map.get(c)) {
+                    return false;
+                }
+                stack.pop();
+            }
+            else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
